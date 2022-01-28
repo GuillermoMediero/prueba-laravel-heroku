367f8b2913350072ef31f61067537068bda856af
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-01-28 12:00:11 +0100

    prueba

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('rol',['admin','jefe','operador','tecnico']);
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,70 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use Facade\FlareClient\Time\SystemTime;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Ascensor;
+use App\Models\Incidencia;
+use App\Models\Modelo;
+use App\Models\Zona;
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      *
-     * @return void 
+     * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        DB::table('productos')->insert([
-            'nombre' => Str:random(10);
-        ])
+
+        Modelo::factory()->count(10)->create();   
+        Zona::factory()->count(10)->create();
+        Ascensor::factory()->count(10)->create();
+    
+        Incidencia::factory()->count(10)->create();
+        //Carbon::now()->format('Y-m-d H:i:s') == Carbon::now()
+            DB::table('users')->insert([
+                'name'=>'Julen',
+                'email' => 'julen@gmail.com',
+                'password' =>Hash::make('adminadmin'),
+                'created_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'updated_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'rol'=> 'admin'
+            ]);
+
+            DB::table('users')->insert([
+                'name'=>'jefe',
+                'email' => 'jefe@gmail.com',
+                'password' =>Hash::make('jefejefe'),
+                'created_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'updated_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'rol'=> 'jefe'
+            ]);
+
+            DB::table('users')->insert([
+                'name'=>'operador',
+                'email' => 'operador@gmail.com',
+                'password' =>Hash::make('operador'),
+                'created_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'updated_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'rol'=> 'operador'
+            ]);
+            
+            DB::table('users')->insert([
+                'name'=>'tecnico',
+                'email' => 'tecnico@gmail.com',
+                'password' =>Hash::make('tecnicotecnico'),
+                'created_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'updated_at'=>  Carbon::now()/*->format('Y-m-d H:i:s')*/,
+                'rol'=> 'tecnico'
+            ]);
+            
+            
+             
     }
 }
