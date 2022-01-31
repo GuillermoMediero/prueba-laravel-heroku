bf8934e312090aa6b2201bda1a2b7f3f2ebbcff3
GuillermoMediero <2gdaw01@arriagainfo.local>
2022-01-31 08:29:43 +0100

    todo

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
-Route::get('/productos/detalles/{id}', [ProductoController::class, 'show'])->name('productos.show');
-Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos.create')->middleware('auth');
-Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
-Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
